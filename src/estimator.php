ae6c8b21e8fc790fbc651578e525c45b84c742ae
olumayokunolayinka <olumayokunolayinka@gmail.com>
2020-04-11 09:34:06 +0100

    Update estimator.php
    challenge one

--- src/estimator.php
+++ src/estimator.php
@@ -2,10 +2,11 @@
 
 function covid19ImpactEstimator($data)
 {
+  // challenge one
   $currentlyInfected      = $data['reportedCases'] * 10;
   $s_currentlyInfected    = $data['reportedCases'] * 50; 
 
-  $day = $input_day / 3;
+  $day = (int) ($input_day / 3);
 
   $infectionsByRequestedTime   = $currentlyInfected   * pow(2,$day);
   $s_infectionsByRequestedTime = $s_currentlyInfected * pow(2,$day);
