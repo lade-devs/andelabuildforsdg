b61187686fc767eb7bc4f2914be7e9cc34f04980
olumayokunolayinka <olumayokunolayinka@gmail.com>
2020-04-11 17:47:18 +0100

    Update estimator.php
    Challenges 1,2,3 done.

--- src/estimator.php
+++ src/estimator.php
@@ -2,9 +2,11 @@
 
 function covid19ImpactEstimator($data)
 {
-  $periodType   = $data['periodType'];
-  $timeToElapse = $data['timeToElapse'];
-  $totalHospitalBeds = $data['totalHospitalBeds'];
+  $periodType          = $data['periodType'];
+  $timeToElapse        = $data['timeToElapse'];
+  $totalHospitalBeds   = $data['totalHospitalBeds'];
+  $region              = json_decode($data['region']);
+  $avgDailyIncomeInUSD = $region['avgDailyIncomeInUSD'];
 
   // challenge one
   $currentlyInfected      = $data['reportedCases'] * 10;
@@ -26,13 +28,48 @@ function covid19ImpactEstimator($data)
    $severeCasesByRequestedTime    = (int) ( (15/100) * $infectionsByRequestedTime );
    $s_severeCasesByRequestedTime  = (int) ( (15/100) * $s_infectionsByRequestedTime );
 
-   $availableBeds                 = (int) ( (35/100) * $totalHospitalBeds );
+   $availableBeds                 = (int) ( (95/100) * $totalHospitalBeds );
+   $availableBeds                 = (int) ( (35/100) * $availableBeds );
    $hospitalBedsByRequestedTime   = $availableBeds - $severeCasesByRequestedTime;
    $s_hospitalBedsByRequestedTime = $availableBeds - $s_severeCasesByRequestedTime;
 
+   // challenge three
+    $casesForICUByRequestedTime   = (int) ( (5/100) * $infectionsByRequestedTime );
+    $s_casesForICUByRequestedTime = (int) ( (5/100) * $s_infectionsByRequestedTime );
+
+    $casesForVentilatorsByRequestedTime   = (int) ( (2/100) * $infectionsByRequestedTime );
+    $s_casesForVentilatorsByRequestedTime = (int) ( (2/100) * $s_infectionsByRequestedTime );
+
+    $dollarsInFlight   = (int) ( ($infectionsByRequestedTime * 0.65 * $avgDailyIncomeInUSD) / $timeToElapse );
+    $s_dollarsInFlight = (int) ( ($s_infectionsByRequestedTime * 0.65 * $avgDailyIncomeInUSD) / $timeToElapse );
+
+    // storing the estimation output
+    $impact[] = [
+        $currentlyInfected,
+        $infectionsByRequestedTime,
+        $severeCasesByRequestedTime,
+        $hospitalBedsByRequestedTime,
+        $casesForICUByRequestedTime,
+        $casesForVentilatorsByRequestedTime,
+        $dollarsInFlight
+    ];
+
+    $severeImpact[] =[
+        $s_currentlyInfected,
+        $s_infectionsByRequestedTime,
+        $s_severeCasesByRequestedTime,
+        $s_hospitalBedsByRequestedTime,
+        $s_casesForICUByRequestedTime,
+        $s_casesForVentilatorsByRequestedTime,
+        $s_dollarsInFlight
+    ];
+
+    // Final output
+    $data[] = [
+        $data,
+        $impact,
+        $severeImpact
+        ];
+
   return $data;
-}
-
-function challenge_one($reportedCases,$day){
-
 }
