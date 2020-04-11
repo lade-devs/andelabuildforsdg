7e8ab0cca1648420ee4a4a94e996f6c6182e6253
olumayokunolayinka <olumayokunolayinka@gmail.com>
2020-04-11 15:23:17 +0100

    Update estimator.php
    working

--- src/estimator.php
+++ src/estimator.php
@@ -2,11 +2,20 @@
 
 function covid19ImpactEstimator($data)
 {
+  $periodType   = $data['periodType'];
+  $timeToElapse = $data['timeToElapse'];
+
   // challenge one
   $currentlyInfected      = $data['reportedCases'] * 10;
   $s_currentlyInfected    = $data['reportedCases'] * 50; 
 
-  $day = (int) ($input_day / 3);
+  if($periodType == "month"){
+    $timeToElapse *= 30;
+  }elseif($periodType == "week"){
+    $timeToElapse *=7;
+  }
+
+  $day = (int) ($timeToElapse / 3);
 
   $infectionsByRequestedTime   = $currentlyInfected   * pow(2,$day);
   $s_infectionsByRequestedTime = $s_currentlyInfected * pow(2,$day);
