21962def7d412415f14622a1118a380b4ea315e3
olumayokunolayinka <olumayokunolayinka@gmail.com>
2020-04-13 22:13:15 +0100

    11

--- src/estimator.php
+++ src/estimator.php
@@ -38,53 +38,54 @@ function covid19ImpactEstimator($data)
       'impact' => $impact
   ];
 
+    $data = json_encode($data);
 
    // challenge two
-   $severeCasesByRequestedTime    = (int) ( (15/100) * $infectionsByRequestedTime );
-   $s_severeCasesByRequestedTime  = (int) ( (15/100) * $s_infectionsByRequestedTime );
-
-   $availableBeds                 = (int) ( (95/100) * $totalHospitalBeds );
-   $availableBeds                 = (int) ( (35/100) * $availableBeds );
-   $hospitalBedsByRequestedTime   = $availableBeds - $severeCasesByRequestedTime;
-   $s_hospitalBedsByRequestedTime = $availableBeds - $s_severeCasesByRequestedTime;
-
-   // challenge three
-    $casesForICUByRequestedTime   = (int) ( (5/100) * $infectionsByRequestedTime );
-    $s_casesForICUByRequestedTime = (int) ( (5/100) * $s_infectionsByRequestedTime );
-
-    $casesForVentilatorsByRequestedTime   = (int) ( (2/100) * $infectionsByRequestedTime );
-    $s_casesForVentilatorsByRequestedTime = (int) ( (2/100) * $s_infectionsByRequestedTime );
-
-    $dollarsInFlight   = (int) ( ($infectionsByRequestedTime * 0.65 * $avgDailyIncomeInUSD) / $timeToElapse );
-    $s_dollarsInFlight = (int) ( ($s_infectionsByRequestedTime * 0.65 * $avgDailyIncomeInUSD) / $timeToElapse );
-
-    // storing the estimation output
-    $impact[] = [
-        $currentlyInfected,
-        $infectionsByRequestedTime,
-        $severeCasesByRequestedTime,
-        $hospitalBedsByRequestedTime,
-        $casesForICUByRequestedTime,
-        $casesForVentilatorsByRequestedTime,
-        $dollarsInFlight
-    ];
-
-    $severeImpact[] =[
-        $s_currentlyInfected,
-        $s_infectionsByRequestedTime,
-        $s_severeCasesByRequestedTime,
-        $s_hospitalBedsByRequestedTime,
-        $s_casesForICUByRequestedTime,
-        $s_casesForVentilatorsByRequestedTime,
-        $s_dollarsInFlight
-    ];
-
-    // Final output
-    $data[] = [
-        $data,
-        $impact,
-        $severeImpact
-        ];
+//   $severeCasesByRequestedTime    = (int) ( (15/100) * $infectionsByRequestedTime );
+//   $s_severeCasesByRequestedTime  = (int) ( (15/100) * $s_infectionsByRequestedTime );
+//
+//   $availableBeds                 = (int) ( (95/100) * $totalHospitalBeds );
+//   $availableBeds                 = (int) ( (35/100) * $availableBeds );
+//   $hospitalBedsByRequestedTime   = $availableBeds - $severeCasesByRequestedTime;
+//   $s_hospitalBedsByRequestedTime = $availableBeds - $s_severeCasesByRequestedTime;
+//
+//   // challenge three
+//    $casesForICUByRequestedTime   = (int) ( (5/100) * $infectionsByRequestedTime );
+//    $s_casesForICUByRequestedTime = (int) ( (5/100) * $s_infectionsByRequestedTime );
+//
+//    $casesForVentilatorsByRequestedTime   = (int) ( (2/100) * $infectionsByRequestedTime );
+//    $s_casesForVentilatorsByRequestedTime = (int) ( (2/100) * $s_infectionsByRequestedTime );
+//
+//    $dollarsInFlight   = (int) ( ($infectionsByRequestedTime * 0.65 * $avgDailyIncomeInUSD) / $timeToElapse );
+//    $s_dollarsInFlight = (int) ( ($s_infectionsByRequestedTime * 0.65 * $avgDailyIncomeInUSD) / $timeToElapse );
+//
+//    // storing the estimation output
+//    $impact[] = [
+//        $currentlyInfected,
+//        $infectionsByRequestedTime,
+//        $severeCasesByRequestedTime,
+//        $hospitalBedsByRequestedTime,
+//        $casesForICUByRequestedTime,
+//        $casesForVentilatorsByRequestedTime,
+//        $dollarsInFlight
+//    ];
+//
+//    $severeImpact[] =[
+//        $s_currentlyInfected,
+//        $s_infectionsByRequestedTime,
+//        $s_severeCasesByRequestedTime,
+//        $s_hospitalBedsByRequestedTime,
+//        $s_casesForICUByRequestedTime,
+//        $s_casesForVentilatorsByRequestedTime,
+//        $s_dollarsInFlight
+//    ];
+//
+//    // Final output
+//    $data[] = [
+//        $data,
+//        $impact,
+//        $severeImpact
+//        ];
 
   return $data;
 }
